fe34cc3ea3609789fa3717eaf5794c35193902ba
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-23 21:50:45 +0300

    Separate realiazaton to threeclasses
    One realization for display health, other for display healerHealt

--- Assets/Scripts/UI/DisplayHealth.cs
+++ Assets/Scripts/UI/DisplayHealth.cs
@@ -2,31 +2,19 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class DisplayHealth : MonoBehaviour
-{
-    [Tooltip("Set to the child text this tag if you need to display the current health")]
-    [SerializeField] private string _tagName = "HealthText";
+public class DisplayHealth : DisplayOnText {
+    private HealthComponent _health;
 
-    private Text[] _texts;
-    private HealthComponent _healthComponent;
-
-    private void Awake() {
-        _texts = gameObject.GetComponentsInChildren<Text>();
-        _healthComponent = gameObject.GetComponentInParent<HealthComponent>();
-        UpdateHealth(this, _healthComponent.Health);
-        _healthComponent.CurrentHealth += UpdateHealth;
+    // Subscribe to object with health
+    protected override void Awake() {
+        base.Awake();
+        _health = gameObject.GetComponentInParent<HealthComponent>();
+        _health.CurrentHealth += UpdateText;
+        UpdateText(this, _health.Health);
     }
 
-    private void OnDestroy() {
-        _healthComponent.CurrentHealth -= UpdateHealth;
-    }
-
-    // Set to each text the current sender health
-    private void UpdateHealth(object sender, float currentHealth) {
-        foreach (var textComponent in _texts) {
-            if (textComponent.tag == _tagName) {
-                textComponent.text = ((int)currentHealth).ToString();
-            }
-        }
+    // Unsubscribe
+    protected void OnDestroy() {
+        _health.CurrentHealth -= UpdateText;
     }
 }
---
+++ Assets/Scripts/UI/DisplayOnText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayOnText : MonoBehaviour
+{
+    [Tooltip("Set to the child text this tag if you need to display the current health")]
+    [SerializeField] protected string _tagName = "HealthText";
+
+    protected Text[] _texts;
+
+    // Find all text objects at childrens
+    protected virtual void Awake() {
+        _texts = gameObject.GetComponentsInChildren<Text>();
+    }
+
+    // Set to each text the current sender health
+    protected void UpdateText(object sender, float value) {
+        foreach (var textComponent in _texts) {
+            if (textComponent.tag == _tagName) {
+                textComponent.text = ((int)value).ToString();
+            }
+        }
+    }
+}
