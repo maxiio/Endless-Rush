9f34576dfd12698b0cd48e16802673ff929ebfee
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-23 21:52:05 +0300

    Add healer script
    Contain value to heal.
    When object with tag is comming into trigger then call the heal method of this object.

---
+++ Assets/Scripts/Healer/Healer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Healer : MonoBehaviour
+{
+    [SerializeField] private float _healthToHeal;
+    [SerializeField] private string _playerTag = "Player";
+
+    public float HealthToHeal { get => _healthToHeal; private set { } }
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == _playerTag) {
+            HealthComponent playerHealth = collider.gameObject.GetComponent<HealthComponent>();
+            playerHealth.Heal(_healthToHeal);
+
+            Destroy(gameObject);
+        }
+    }
+}
