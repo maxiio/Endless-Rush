f5f108a98399c13a66e08454e58f2c0f85052312
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-24 13:28:04 +0300

    Update camera
    Add functionality to choose for using default position or use offset position

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class CameraFollow : MonoBehaviour
 {
     [Header("Object to follow")]
     [SerializeField] private Transform _target;
-    [SerializeField] private Vector3 _positionOffset;
     [SerializeField] private float _lerpSpeed;
+
+    [SerializeField] private bool _IsUsePositionOffset;
+    [SerializeField] private Vector3 _positionOffset;
+
 
     private Vector3 _lerpedPosition;
 
@@ -15,15 +17,25 @@ public class CameraFollow : MonoBehaviour
         if (!_target) {            
             throw new Exception("No target at camera");
         }
+
+        // Set to offset the start default camera position
+        if (!_IsUsePositionOffset) {
+            _positionOffset = gameObject.transform.position;
+        }
+
+        CalculateNewPosition();
     }
 
-    // Calculate new position
-    void FixedUpdate() {
+    void CalculateNewPosition() {
         _lerpedPosition = Vector3.Lerp(transform.position, _target.position + _positionOffset, Time.deltaTime * _lerpSpeed);
+    }
+
+    void FixedUpdate() {
+        CalculateNewPosition();
     }
 
     // Set camera position after all other object updates
     void LateUpdate() { 
-        transform.position = _lerpedPosition;
+       transform.position = _lerpedPosition;
     }
 }
