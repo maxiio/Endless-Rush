f43fea7423bbc3fd7a6b66a6836655a280d5bc2c
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-24 13:25:58 +0300

    Text can rotate to camera rotation
    Text can equal the camera rotation to watch in one direction.

--- Assets/Scripts/UI/DisplayOnText.cs
+++ Assets/Scripts/UI/DisplayOnText.cs
@@ -7,28 +7,28 @@ public class DisplayOnText : MonoBehaviour {
     [Tooltip("Set to the child text this tag if you need to display the current health")]
     [SerializeField] protected string _tagName = "HealthText";
 
-    [SerializeField] protected Transform _cameraTransform;
-    [SerializeField] protected string _cameraTag = "MainCamera";
+    [Header("Text look at camera")]
     [Tooltip("Set rotation of object to camera movement direction")]
     [SerializeField] protected bool _isCameraRotationDirection;
-    [SerializeField] protected bool _isXAutorotation;
-    [SerializeField] protected bool _isYAutorotation;
-    [SerializeField] protected bool _isZAutorotation;
+    [SerializeField] protected Transform _cameraTransform;
+    [SerializeField] protected string _cameraTag = "MainCamera";
 
     protected Text[] _texts;
 
     // Find all text objects at childrens
     protected virtual void Awake() {
         _texts = gameObject.GetComponentsInChildren<Text>();
-        RotateTextsToCamera();
+
+        SetCameraTransform();
+        if (_isCameraRotationDirection) {
+            RotateTextsToCamera();        
+        }
     }
 
     // Each text object look at camera
     private void RotateTextsToCamera() {
-        SetCameraTransform();
-
         foreach (var text in _texts) {
-            RotateTextToCamera(text);
+            text.transform.rotation = _cameraTransform.rotation;
         }
     }
 
@@ -39,27 +39,7 @@ public class DisplayOnText : MonoBehaviour {
             throw new Exception("On scene more then 2 cameras. Set the camera to DisplayOnText.cs");
         }
         _cameraTransform = cameras[0].transform;
-    }
-
-    // Rotate text object to camera by axis
-    private void RotateTextToCamera(Text text) {
-        if (_isCameraRotationDirection) {
-            text.transform.rotation = _cameraTransform.rotation;
-        }
-
-        Vector3 positionToRotate = text.transform.position;
-        if (_isXAutorotation) {
-            positionToRotate.x = _cameraTransform.position.x;
-        }
-        if (_isYAutorotation) {
-            positionToRotate.y = _cameraTransform.position.y;
-        }
-        if (_isZAutorotation) {
-            positionToRotate.z = _cameraTransform.position.z;
-        }
-        
-        text.transform.LookAt(positionToRotate);
-    }
+    }    
 
     // Set to each text the current sender health
     protected void UpdateText(object sender, float value) {
