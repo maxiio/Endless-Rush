c403e7a7ca821a5db4f060945e7a1da6828d4dad
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-25 14:49:04 +0300

    Create ButtonAction
    The script which handle the actions from button.
    Contain the all types of actions.

---
+++ Assets/Scripts/UI/ScreenTransition/Button/ButtonAction.cs
@@ -0,0 +1,64 @@
+﻿public class ButtonAction
+{
+    // The all actions which button's can send
+    public enum Actions {
+        NULL,
+        PLAY,
+        PAUSE,
+        MENU,
+        RESTART,
+        CONTINUE,
+        EXIT,
+        GAMEOVER,
+        REVIVE
+    }
+
+    public static void HandleAction(Actions action) {
+        switch (action) {
+            case Actions.NULL:
+                throw new System.Exception("Handle the Null action");
+            case Actions.RESTART:
+            case Actions.PLAY:
+                RestartPlay();
+                UnPause();
+                break;
+            case Actions.CONTINUE:
+                ContinuePlay();
+                UnPause();
+                break;
+            case Actions.REVIVE:
+                Revive();
+                UnPause();
+                break;
+            case Actions.MENU:
+            case Actions.GAMEOVER:
+            case Actions.PAUSE:
+                Pause();
+                break;
+            case Actions.EXIT:
+                Pause();
+                Exit();
+                break;            
+            default:
+                throw new System.Exception("Handle the default action");
+        }
+    }
+
+    private static void RestartPlay() {
+    }
+
+    private static void ContinuePlay() {
+    }
+
+    private static void Revive() {
+    }
+
+    private static void Pause() {
+    }
+
+    private static void UnPause() {
+    }
+
+    private static void Exit() {
+    }
+}
