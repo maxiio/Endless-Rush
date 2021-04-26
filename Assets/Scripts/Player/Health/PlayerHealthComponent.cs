eb9a1354c29b835e1f1f423cbce47d9e9b289c9b
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-26 22:44:12 +0300

    Update PlayerHealthComponent.cs
    Check the dead

--- Assets/Scripts/Player/Health/PlayerHealthComponent.cs
+++ Assets/Scripts/Player/Health/PlayerHealthComponent.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerHealthComponent : HealthComponent
 {
 	public static event EventHandler<float> AmountOfDicreasedHealth;
 
 	public override void Hit(float damage) {
-		base.Hit(damage);
-		AmountOfDicreasedHealth?.Invoke(this, damage);
+		if (!IsDie) {
+			base.Hit(damage);
+			AmountOfDicreasedHealth?.Invoke(this, damage);
+        }        
 	}
 }
