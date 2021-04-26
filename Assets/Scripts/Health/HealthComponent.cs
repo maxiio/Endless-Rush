e16fbba6eba23db84c76e3cf1f57620c58808aa2
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-26 16:25:13 +0300

    Update HealthComponent.cs
    Try to add the SetDefaultHealth.
    Change the Destroy option.

--- Assets/Scripts/Health/HealthComponent.cs
+++ Assets/Scripts/Health/HealthComponent.cs
@@ -9,6 +9,8 @@ public class HealthComponent : MonoBehaviour
 	[SerializeField] protected float _health;
 	[SerializeField] protected bool _isDestroyed = false;
 
+	private float _defaultHealth;
+
 	public virtual float Health {
 		get => _health;
 		protected set {
@@ -17,17 +19,36 @@ public class HealthComponent : MonoBehaviour
 			if (_health < 1) {
 				IsDie?.Invoke(this, EventArgs.Empty);
 				if (_isDestroyed) {
-					Destroy(gameObject);
+					DestroyObject(gameObject);
                 }
 			}
 		}
 	}
 
-	public virtual void Hit(float damage) {
+    private void Awake() {
+		_defaultHealth = _health;
+	}
+
+    public virtual void Hit(float damage) {
 		Health -= damage;
 	}
 
 	public void Heal(float health) {
 		Health += health;
     }
+
+	private void StroyObject(GameObject gameObject) {
+		gameObject.GetComponentInChildren<Collider>().enabled = true;
+		gameObject.GetComponentInChildren<Renderer>().enabled = true;
+	}
+
+	private void DestroyObject(GameObject gameObject) {
+		gameObject.GetComponentInChildren<Collider>().enabled = false;
+		gameObject.GetComponentInChildren<Renderer>().enabled = false;
+	}
+
+    public void SetDefaultHealth(GameObject gameObject) {
+		StroyObject(gameObject);
+		Health = _defaultHealth;
+	}
 }
