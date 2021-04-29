10443047d00c294cca1873946d8b7f76c56c3290
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-30 00:38:13 +0300

    Create LevelGenerator.cs
    Temporary version. Spawn objects throw the time and destroy after time. Spawn forward from the player.

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject[] _spawnedObjects;
+    [SerializeField] private float _delayOfGeneration = 2f;
+    [SerializeField] private float _xSpawnPositionOffsetFromPlayer;
+    [SerializeField] private float _zSpawnPosition = 0f;
+    
+    private WaitForSeconds _destroyDelay = new WaitForSeconds(10f);
+    private IEnumerator<WaitForSeconds> _levelGeneratorCoroutine;
+
+    private Vector3 GetSpawnPosition() {
+        Vector3 spawnPosition = _player.transform.position;
+        spawnPosition.x += _xSpawnPositionOffsetFromPlayer;
+        spawnPosition.z = _zSpawnPosition;
+        return spawnPosition;
+    }
+
+    IEnumerator<WaitForSeconds> GenerateLevel() {
+        Vector3 spawnPosition;
+        while (true) {
+            yield return new WaitForSeconds(_delayOfGeneration);
+
+            Debug.Log("Spawn");
+
+            float playerHealth = _player.GetComponent<HealthComponent>().Health;
+            //float healthDifference = 1f;
+            //_spawnedObject.GetComponent<HealthComponent>().Health = playerHealth - healthDifference;
+            foreach (var _spawnedObject in _spawnedObjects) {
+                GameObject newBlock = Instantiate(_spawnedObject);
+                newBlock.transform.position = GetSpawnPosition();
+
+                StartCoroutine(DestroyAfterTime(newBlock));
+            }
+            
+        }
+    }
+
+    IEnumerator<WaitForSeconds> DestroyAfterTime(GameObject destroyedObject) {
+        yield return _destroyDelay;
+        Destroy(destroyedObject);
+    }
+
+    private void Start() {
+        _levelGeneratorCoroutine = GenerateLevel();
+        StartCoroutine(_levelGeneratorCoroutine);
+    }
+
+    private void OnDestroy() {
+        StopCoroutine(_levelGeneratorCoroutine);
+    }
+}
