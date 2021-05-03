de79af01322052cca2e2b4920eef12f5001e8734
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-03 13:04:05 +0300

    Create HealerHealth.cs
    Move the old realization from Healer to this and change the damage system.

---
+++ Assets/Scripts/Healer/HealerHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HealerHealth : HealthComponent
+{
+    [SerializeField] private string _playerTag = "Player";
+
+    // No hits to healer
+    public override void Hit(float damage) {}
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == _playerTag) {
+            HealthComponent playerHealth = collider.gameObject.GetComponent<HealthComponent>();
+            playerHealth.Heal(Health);
+
+            DestroyObject(gameObject);
+        }
+    }
+}
