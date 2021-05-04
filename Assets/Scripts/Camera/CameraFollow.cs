19ced85449800f7d783af1d8c52738e5fe183002
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-04 18:16:54 +0300

    Add camera freeze to axis
    I need to freeze the z axis

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Serializable]
+    struct Axis {
+        public bool x;
+        public bool y;
+        public bool z;
+    }
+
     [Header("Object to follow")]
     [SerializeField] private Transform _target;
+    [SerializeField] private Axis _freezeAxis;
     [SerializeField] private float _lerpSpeed;
 
     [SerializeField] private bool _IsUsePositionOffset;
     [SerializeField] private Vector3 _positionOffset;
 
-
     private Vector3 _lerpedPosition;
+    private Vector3 _defaultPosition;
 
     private void Awake() {
         if (!_target) {            
@@ -24,10 +32,24 @@ public class CameraFollow : MonoBehaviour
         }
 
         CalculateNewPosition();
+        _defaultPosition = _lerpedPosition;
     }
 
-    void CalculateNewPosition() {
+    private void CalculateNewPosition() {
         _lerpedPosition = Vector3.Lerp(transform.position, _target.position + _positionOffset, Time.deltaTime * _lerpSpeed);
+        FreezeCameraMovementAxis(ref _lerpedPosition);
+    }
+
+    private void FreezeCameraMovementAxis(ref Vector3 position) {
+        if (_freezeAxis.x) {
+            position.x = _defaultPosition.x;
+        }
+        if (_freezeAxis.y) {
+            position.y = _defaultPosition.y;
+        }
+        if (_freezeAxis.z) {
+            position.z = _defaultPosition.z;
+        }
     }
 
     void FixedUpdate() {
