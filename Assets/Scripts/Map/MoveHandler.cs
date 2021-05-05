9596e4d4720cde8a9c9628b0b0b7dbf2d1d8062c
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-05 22:12:11 +0300

    Create MoveHandler.cs
    Teleport maps forward from player

---
+++ Assets/Scripts/Map/MoveHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    [Range(0, size / 2)]
+    private float moveX;
+
+    const int size = 100;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player.transform.position.x > transform.position.x + size / 2 + moveX) {
+            Move();
+        }
+    }
+
+    private void Move() {
+        transform.Translate(size * 2, 0, 0);
+    }
+}
