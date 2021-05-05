abba44d7be7a9f7e562b4821a7b89c861ae23fe6
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-05 21:16:20 +0300

    Update TeleportToPlayer.cs
    Add teleporting by coordinates but this is also bad.

--- Assets/TeleportToPlayer.cs
+++ Assets/TeleportToPlayer.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class TeleportToPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _objectToTeleport;
-    Vector3 _positionToTeleport;
+    [SerializeField] private float _distanceToTriggerTeleport;
+
+    private Vector3 _positionToTeleport;
+    private Vector3 _objectLastPosition;
 
     private void Start() {
         _positionToTeleport = _objectToTeleport.position;
         // Difference with object to teleport
-        _positionToTeleport.y -= 0.1f;
+        _positionToTeleport.y -= 0.5f;
+
+        _objectLastPosition = _objectToTeleport.position;
+    }
+
+    private void TeleportToObject() {
+        _positionToTeleport.x = _objectToTeleport.position.x;
+        gameObject.transform.position = _positionToTeleport;
     }
 
     private void FixedUpdate()
     {
-        _positionToTeleport.x = _objectToTeleport.position.x;
-        gameObject.transform.position = _positionToTeleport;
+        if (_objectLastPosition.x + _distanceToTriggerTeleport < _objectToTeleport.position.x) {
+            TeleportToObject();
+            _objectLastPosition = _objectToTeleport.position;
+        }
     }
 }
