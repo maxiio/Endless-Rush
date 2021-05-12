49104d72c4908b3b85d4f1240c409f286078e28f
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-13 00:42:32 +0300

    Take out the score saving

--- Assets/Scripts/Core/Managers/ScoreSystem/ScoreManager.cs
+++ Assets/Scripts/Core/Managers/ScoreSystem/ScoreManager.cs
@@ -4,87 +4,61 @@ using UnityEngine;
 namespace Core.Managers.ScoreSystem {
 	public class ScoreManager : MonoBehaviour {
 		[SerializeField] private bool resetScore;
+		public float CurrentScore { get; private set; }
+		public float MAXScore { get; private set; }
+		private const float DefaultMaxScore = 0;
+		private ScoreSaver _scoreSaver;
 
-		private float _currentScore;
-		private float _maxScore;
-		private ScoreData _scoreData;
-
-		private float Score {
-			get => _currentScore;
-			set {
-				if (value >= 0) {
-					_currentScore = value;
-					if (_maxScore < _currentScore) {
-						_maxScore = _currentScore;
-						SaveScoreData(_currentScore);
-					}
-				}
-			}
-		}
-
-		public int GetIntScore() {
-			return (int) _currentScore;
-		}
-
-		public int GetIntMaxScore() {
-			return (int) _maxScore;
-		}
-
-		// Save local Score to playerPrefs
-		private void SaveScoreData(float value) {
-			Debug.Log("Save Score");
-			PlayerPrefs.SetFloat(ScoreData.MAXScoreName, value);
-		}
-
-		// Increment the local Score on value
-		private void Add(float amount) {
-			Score += amount;
-		}
-
-		// If we need to restart level
-		public void Reset() {
-			Score = 0;
-		}
-
-		// Set the scoreData from the PlayerPrefs to save playerMaxScore
-		private float GetScoreData() {
-			_scoreData = new ScoreData();
-			if (PlayerPrefs.HasKey(ScoreData.MAXScoreName)) {
-				return _scoreData.maxScore = PlayerPrefs.GetFloat(ScoreData.MAXScoreName);
-			}
-
-			return 0f;
-		}
-
-		// Set local max score to zero
-		private void ResetScore() {
-			_maxScore = 0;
-			Debug.Log("Score is reset!");
-		}
-
-		// Subscribe to changes of health component
 		private void Awake() {
-			// Get the serialized data
-			_maxScore = GetScoreData();
+			_scoreSaver = GetComponent<ScoreSaver>();
 
 			// Reset local max score
 			if (resetScore) {
 				ResetScore();
 			}
 
-			// Subscribe
+			SetMaxScore(_scoreSaver.Score);
+			Subscribe();
+		}
+
+		private void ResetScore() {
+			SetMaxScore(DefaultMaxScore);
+			SaveScore(DefaultMaxScore);
+			Debug.Log("Score is reset!");
+		}
+
+		private void SetMaxScore(float score) {
+			MAXScore = score;
+		}
+
+		private void Subscribe() {
 			PlayerHealthComponent.AmountOfHealthToDecrease += AddScoreListener;
 		}
 
 		// Unsubscribe to changers of health component
 		private void OnDestroy() {
-			// Unsubscribe
+			Unsubscribe();
+		}
+
+		private void Unsubscribe() {
 			PlayerHealthComponent.AmountOfHealthToDecrease -= AddScoreListener;
 		}
 
 		// Invoke the Score function - Add
 		private void AddScoreListener(object sender, float amountOfChangedHealth) {
 			Add(amountOfChangedHealth);
+		}
+
+		private void Add(float amount) {
+			CurrentScore += amount;
+			if (CurrentScore > MAXScore) {
+				SetMaxScore(CurrentScore);
+				SaveScore(CurrentScore);
+			}
+		}
+
+		private void SaveScore(float scoreToSave) {
+			_scoreSaver.Score = scoreToSave;
 		}
 	}
 }
