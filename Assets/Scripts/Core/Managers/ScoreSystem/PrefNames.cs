2ec526a62e790ec209c6ec4dea13a758d686ef86
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-13 00:43:41 +0300

    Replace the ScoreData to PrefNames
    Make from ScoreData to the static class with player pref names.

--- Assets/Scripts/Core/Managers/ScoreSystem/PrefNames.cs
+++ Assets/Scripts/Core/Managers/ScoreSystem/PrefNames.cs
@@ -3,8 +3,7 @@
 namespace Core.Managers.ScoreSystem {
 	// Class which hold the maxScore variable and him name
 	[Serializable]
-	public class ScoreData {
+	public static class PrefNames {
 		public static string MAXScoreName = "maxScore";
-		public float maxScore;
 	}
 }
