2f25b252be456f7afeb21d28dde9a308234bec10
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-13 14:46:09 +0300

    Delete PrefNames  class
    The name of variable to save or get is setting at other place.

--- Assets/Scripts/SaveData/PrefNames.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SaveData {
-	// Class which hold the maxScore variable and him name
-	[Serializable]
-	public static class PrefNames {
-		public static string MAXScoreName = nameof(MAXScoreName);
-	}
-}
