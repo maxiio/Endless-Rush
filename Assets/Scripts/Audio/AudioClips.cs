f21344488826b50efc0fc9009f3810e1c4f1e6b9
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-15 21:40:34 +0300

    Make AudioClips ScriptableObject
    Contain the information about clips, links to audio clips and action when we should to play clip.

--- Assets/Scripts/Audio/AudioClips.cs
+++ Assets/Scripts/Audio/AudioClips.cs
@@ -1,25 +1,29 @@
 ﻿using System;
+using System.Collections;
+using UI.ScreenTransition.Button;
 using UnityEngine;
 
 namespace Audio {
 	[CreateAssetMenu(fileName = "AudioClips", menuName = "ScriptableObjects/Audio")]
 	public class AudioClips : ScriptableObject {
 		[Serializable]
-		public struct ClipDictionary {
-			public ClipName clipName;
+		public struct ClipData {
+			public ButtonAction.Actions action;
 			public AudioClip audioClip;
+			public bool isLoop;
 		}
 
-		public enum ClipName {
-			NONE,
-			START_MENU,
-			PAUSE_MENU,
-			GAME,
-			GAMEOVER,
-			HIT,
-			HEAL
+		public ClipData[] clips;
+
+		public ClipData GetClipByAction(ButtonAction.Actions action) {
+			foreach (var clip in clips) {
+				if (action == clip.action) {
+					return clip;
+				}
+			}
+
+			Debug.LogError($"No clip with action {action}");
+			return default;
 		}
-		
-		public ClipDictionary[] clips;
 	}
 }
