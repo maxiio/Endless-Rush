ae06e95983798dda1e5de77093ebf53689d4bccb
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-15 21:46:38 +0300

    Create EasterEgg
    Hit the player if he'll touch this. Random deletion of this egg.

---
+++ Assets/Scripts/Core/Objects/EasterEgg/RandomDeletion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Core.Objects.EasterEgg {
+	internal class RandomDeletion : MonoBehaviour {
+		[SerializeField] private float chanceToDestroy = 0.9f;
+		private readonly int _maxValue = 1000;
+
+		private void Start() {
+			var rand = new System.Random();
+			var isDestroyed = rand.Next(_maxValue) < chanceToDestroy * _maxValue;
+			if (isDestroyed) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
