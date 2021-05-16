8fa8712d9fb895e7956aafdfdc56feda2f1f456e
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-16 20:29:52 +0300

    Make the SpeedController interface
    This interface is  getting the current speed and response for returning the calculated speed.

---
+++ Assets/Scripts/Core/Player/Movement/XAxisSpeedController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Core.Player.Movement {
+	public class XAxisSpeedController : MonoBehaviour, ISpeedController {
+		[SerializeField] private float maxXAxisSpeed;
+
+		[SerializeField] [Range(.2f, 10)]
+		private float lerpSpeed;
+
+		private Vector3 _movementSpeed;
+
+		private void Awake() {
+			SetMovementSpeed();
+		}
+
+		private void SetMovementSpeed() {
+			_movementSpeed.x = maxXAxisSpeed;
+		}
+
+		public Vector3 GetVelocity(Vector3 currentVelocity) {
+			return Vector3.Lerp(currentVelocity, _movementSpeed, Time.deltaTime * lerpSpeed);
+		}
+	}
+}
